8c322b142a403fdc651d8bc8b7f8f0e3a73ff396
imkdw <imkdw@kakao.com>
2025-02-27 23:09:43 +0900

    chore: create pretterrc.json

--- packages/config/eslint.config.mjs
+++ packages/config/eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,7 +18,7 @@ const eslintConfig = [
       "react/react-in-jsx-scope": "off",
       "react/no-unknown-property": ["error", { ignore: ["css"] }],
       "@typescript-eslint/no-empty-interface": 0,
-      "@typescript-eslint/no-empty-function": 0,
+      "@typescript-eslint/no-empty-function": 0, 
     },
     settings: {
       react: {
