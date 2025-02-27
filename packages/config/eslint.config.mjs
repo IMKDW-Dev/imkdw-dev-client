279a7db5e939893ac908edb5badc7646d7067d1d
imkdw <imkdw@kakao.com>
2025-02-27 23:57:45 +0900

    chore: change prettirrc.json to rootDir

--- packages/config/eslint.config.mjs
+++ packages/config/eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import typescript from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,21 +13,38 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
+    ignores: ["**/.next/**", "**/node_modules/**", "**/dist/**"],
+  },
+  {
     files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescript,
+    },
     rules: {
       "react/prop-types": 0,
       "no-empty-function": "off",
       "react/react-in-jsx-scope": "off",
       "react/no-unknown-property": ["error", { ignore: ["css"] }],
       "@typescript-eslint/no-empty-interface": 0,
-      "@typescript-eslint/no-empty-function": 0, 
+      "@typescript-eslint/no-empty-function": 0,
+      "@typescript-eslint/no-unused-vars": "off",
+      "@next/next/no-assign-module-variable": "off",
     },
     settings: {
       react: {
         version: "detect",
       },
     },
-    ignores: [".next/**", "node_modules/**"],
   },
   ...compat.extends("next/core-web-vitals", "prettier"),
 ];
--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,3 +1,14 @@
 import config from "@imkdw-dev-client/config/eslint.config.mjs";
 
-export default config;
+const modifiedConfig = [...config];
+if (modifiedConfig.length > 0) {
+  const lastConfig = modifiedConfig[modifiedConfig.length - 1];
+  if (lastConfig.rules) {
+    lastConfig.rules = {
+      ...lastConfig.rules,
+      "@next/next/no-html-link-for-pages": "off",
+    };
+  }
+}
+
+export default modifiedConfig;
