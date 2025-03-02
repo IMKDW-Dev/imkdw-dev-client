456f91f8b4948828339a23b213bff510ff9660fa
dongwoo <imkdw@kakao.com>
2025-03-02 12:39:02 +0900

    feat: ui 내부에 button 생성

--- apps/blog-web/src/app/page.tsx
+++ apps/blog-web/src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@imkdw-dev-client/ui/components/ui/button";
+
 export default function Page() {
-  return <div className="bg-red-500">Hello</div>;
+  return (
+    <div className="bg-red-500">
+      <Button>Click me</Button>
+    </div>
+  );
 }
