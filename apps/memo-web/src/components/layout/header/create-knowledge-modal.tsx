82c6e59baf954981867d08e93cd0270991b09b6a
imkdw <imkdw@kakao.com>
2025-03-26 23:20:16 +0900

    feat: create write knowledge modal

---
+++ apps/memo-web/src/components/layout/header/create-knowledge-modal.tsx
@@ -0,0 +1,14 @@
+import { X } from 'lucide-react';
+
+interface CreateKnowledgeModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export function CreateKnowledgeModal({ isOpen, onClose }: CreateKnowledgeModalProps) {
+  if (!isOpen) {
+    return null;
+  }
+
+  return <div className="fixed inset-0 bg-opacity-50 flex items-center justify-center z-50"></div>;
+}
--- apps/memo-web/src/components/layout/header/header-create-knowledge.tsx
+++ apps/memo-web/src/components/layout/header/header-create-knowledge.tsx
@@ -1,12 +1,24 @@
+'use client';
+
 import { PencilLine } from 'lucide-react';
+import { useState } from 'react';
+import { CreateKnowledgeModal } from './create-knowledge-modal';
 
 export function HeaderCreateKnowledge() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <button className="flex items-center gap-2 rounded-lg bg-white p-4 shadow-primary flex-1">
-      <PencilLine className="text-gray-400" fontSize={28} />
-      <div className="flex items-center h-full">
-        <span className="text-gray-400">Write Knowledge</span>
-      </div>
-    </button>
+    <>
+      <button
+        className="flex items-center gap-2 rounded-lg bg-white p-4 shadow-primary flex-1"
+        onClick={() => setIsModalOpen(true)}
+      >
+        <PencilLine className="text-gray-400" fontSize={28} />
+        <div className="flex items-center h-full">
+          <span className="text-gray-400">Write Knowledge</span>
+        </div>
+      </button>
+      <CreateKnowledgeModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+    </>
   );
 }
